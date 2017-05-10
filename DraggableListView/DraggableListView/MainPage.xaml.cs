bf1a97c8b4818f28892402e1bb83957c43468e93
dpineda <desi.pineda.1@gmail.com>
2017-05-10 00:35:58 -0500

    Update xamarin forms, iOS renderers progress

--- DraggableListView/DraggableListView/MainPage.xaml.cs
+++ DraggableListView/DraggableListView/MainPage.xaml.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
 
 namespace DraggableListView
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<CellItem> Items1 { get; set; }
         public MainPage()
         {
+			Items1 = new ObservableCollection<CellItem>(new List<CellItem>() {
+				new CellItem(){Name="Test1", Height=30.0},
+				new CellItem(){Name="Test2", Height=60.0},
+				new CellItem(){Name="Test3", Height=20.0},
+			});
             InitializeComponent();
+            this.BindingContext = this;
         }
+
     }
+
+	public class CellItem
+	{
+		public string Name { set; get; }
+		public double Height { set; get; }
+	}
 }
